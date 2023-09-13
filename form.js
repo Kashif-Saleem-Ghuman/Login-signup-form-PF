e54943b87ba4b802e11569a6196ceb1dd6a3e760
kashif <kashif_saleem_23@hotmail.com>
2023-09-13 21:05:54 +0500

    Feat: toggled icon for password hide and show with hide and show icon

--- form.js
+++ form.js
@@ -10,45 +10,65 @@ passShowHide.forEach((icon) => {
     let pwField = icon.previousElementSibling;
     if (pwField.type === "password") {
       pwField.type = "text";
-      icon.classList.add("fa-eye-slash");
+
+      icon.classList.replace("bxs-hide", "bx-show");
     } else {
       pwField.type = "password";
-      icon.classList.remove("fa-eye-slash");
+      icon.classList.replace("bx-show", "bxs-hide");
     }
   });
 });
 
-// Add data to local storage
+// Add data to local storage for Login
 
-window.onload = () => {
-  // Check local storage support
-  if (localStorage) {
-    // Add event listener to form submission for Login
-    document.getElementById("LoginForm").addEventListener("submit", (e) => {
-      e.preventDefault();
-      // Get the data from the form for Login
-      let email = document.getElementById("email").value;
-      console.log(email);
-      let password = document.getElementById("passwordLogin").value;
+const addLoginData = () => {
+  // Add event listener to form submission for Login
+  document.getElementById("LoginForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    // Get the data from the form for Login
+    let email = document.getElementById("email").value;
+    console.log(email);
+    let password = document.getElementById("passwordLogin").value;
 
+    //vaildate if the user has entered data
+
+    if (email === "" || password === "") {
+      alert("Please fill in all fields");
+      return false;
+    } else {
       // Save the data to localStorage for Login
       localStorage.setItem("email-Login", email);
       localStorage.setItem("password-Login", password);
+    }
+  });
+};
 
-      alert("Login data has been saved to local storage.");
-    });
+// Add data to local storage for Sign Up
 
-    // Add event listener to form submission for Sign Up
-    document.getElementById("signupForm").addEventListener("submit", (e) => {
-      e.preventDefault();
-      // Get the data from the form for Sign Up
-      let firstName = document.getElementById("firstName").value;
-      let lastName = document.getElementById("lastName").value;
-      let signUpEmail = document.getElementById("signupEmail").value;
-      let signUpPassword = document.getElementById("signupPassword").value;
-      let address = document.getElementById("address").value;
-      let phoneNumber = document.getElementById("phoneNumber").value;
+const addSignUpData = () => {
+  document.getElementById("signupForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    // Get the data from the form for Sign Up
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let signUpEmail = document.getElementById("signupEmail").value;
+    let signUpPassword = document.getElementById("signupPassword").value;
+    let address = document.getElementById("address").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
 
+    //validate if the user has entered data
+
+    if (
+      firstName === "" ||
+      lastName === "" ||
+      signUpEmail === "" ||
+      signUpPassword === "" ||
+      address === "" ||
+      phoneNumber === ""
+    ) {
+      alert("Please fill in all fields");
+      return false;
+    } else {
       // Save the data to localStorage for Sign Up
       localStorage.setItem("firstName-SignUp", firstName);
       localStorage.setItem("lastName-SignUp", lastName);
@@ -56,18 +76,6 @@ window.onload = () => {
       localStorage.setItem("password-SignUp", signUpPassword);
       localStorage.setItem("address-SignUp", address);
       localStorage.setItem("phoneNumber-SignUp", phoneNumber);
-
-      alert("Sign Up data has been saved to local storage.");
-    });
-  }
+    }
+  });
 };
-
-// add class clock to sign up on click of sign up button
-
-// const signUpBtn = document.querySelector(".sign-up-btn");
-
-// signUpBtn.addEventListener("click", () => {
-//   forms.classList.add("hide");
-
-//   document.querySelector(".signUpForm").classList.add("show");
-// });
